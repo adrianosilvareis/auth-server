2b8c675c3694c331d34cdfdc2a13719078a5682e
Adriano Reis <adriano_silvareis@hotmail.com>
2021-04-10 15:34:13 -0300

    chore: start config projet

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  clearMocks: true,
+  coverageDirectory: 'coverage',
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1'
+  },
+  preset: 'ts-jest',
+  roots: [
+    '<rootDir>/src'
+  ],
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
