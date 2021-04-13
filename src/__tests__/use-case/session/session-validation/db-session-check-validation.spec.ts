d33412295c742f74fa2646e9877e535c6452214d
Adriano Reis <adriano_silvareis@hotmail.com>
2021-04-12 23:10:43 -0300

    feat: should call get session by id

--- src/__tests__/use-case/session/session-validation/db-session-check-validation.spec.ts
+++ src/__tests__/use-case/session/session-validation/db-session-check-validation.spec.ts
@@ -1,5 +1,15 @@
+import { Session, SessionById } from '@/entity/session'
+import { DbSessionCheckValidation } from '@/use-case/session/session-validation/db-session-check-validation'
+
 describe('DbSessionCheckValidation', () => {
-  it.todo('should call get session by id')
+  it('should call get session by id', async () => {
+    const functionName = 'getById'
+    const expectedCalled = 'any_session_id'
+    const { sut, sessionByIdSut } = makeSut()
+    const spy = jest.spyOn(sessionByIdSut, functionName)
+    await sut.check('any_session_id', 'any_agent_user')
+    expect(spy).toHaveBeenCalledWith(expectedCalled)
+  })
   it.todo('should throw if get session by id throws')
   it.todo('should return true if session not found')
   it.todo('should return true if session is inactive')
@@ -7,3 +17,25 @@ describe('DbSessionCheckValidation', () => {
   it.todo('should return true if the user agent is not the same as the session agent')
   it.todo('should return false if it passes all checks')
 })
+
+type SutTypes = {
+  sut: DbSessionCheckValidation,
+  sessionByIdSut: SessionById
+}
+function makeSut (): SutTypes {
+  const sessionByIdSut = makeSessionByIdStub()
+  const sut = new DbSessionCheckValidation(sessionByIdSut)
+  return {
+    sut,
+    sessionByIdSut
+  }
+}
+
+function makeSessionByIdStub (): SessionById {
+  class SessionByIdStub implements SessionById {
+    async getById (sessionId: string): Promise<Session> {
+      return null
+    }
+  }
+  return new SessionByIdStub()
+}
--- src/use-case/session/session-validation/db-session-check-validation.ts
+++ src/use-case/session/session-validation/db-session-check-validation.ts
@@ -1,7 +1,10 @@
-import { SessionCheckValidation } from '@/entity/session'
+import { SessionById, SessionCheckValidation } from '@/entity/session'
 
 export class DbSessionCheckValidation implements SessionCheckValidation {
+  constructor (private readonly sessionById: SessionById) {}
+
   async check (sessionId: string, userAgent: string): Promise<boolean> {
+    await this.sessionById.getById(sessionId)
     return null
   }
 }
