1c95f4e035574a99c0a20314da0dbc67865c44d4
Adriano Reis <adriano_silvareis@hotmail.com>
2021-04-14 21:59:13 -0300

    feat: should call CreateAuthGroupRepository with correct values

--- src/__tests__/use-case/auth-group/db-create-auth-group.spec.ts
+++ src/__tests__/use-case/auth-group/db-create-auth-group.spec.ts
@@ -1,5 +1,65 @@
+import { AuthGroupProperties, AuthGroup } from '@/entity/auth-group'
+import { uuid } from '@/entity/utils'
+import { DbCreateAuthGroup } from '@/use-case/auth-group/db-create-auth-group'
+import { CreateAuthGroupRepository } from '@/use-case/auth-group/protocols/create-auth-grup-repository'
+import faker from 'faker'
+
 describe('DbCreateAuthGroup', () => {
-  it.todo('should call CreateAuthGroupRepository with correct values')
+  it('should call CreateAuthGroupRepository with correct values', async () => {
+    const { sut, createAuthGroupStub } = makeSut()
+    const functionName = 'create'
+    const spy = jest.spyOn(createAuthGroupStub, functionName)
+    await sut.create(mockedAuthGroupProperties)
+    expect(spy).toHaveBeenCalledWith(mockedAuthGroupProperties)
+  })
   it.todo('should throw if CreateAuthGroupRepository throws')
   it.todo('should return a AuthGroupModel on success')
 })
+
+type SutTypes = {
+  sut: DbCreateAuthGroup,
+  createAuthGroupStub: CreateAuthGroupRepository
+}
+
+function makeSut (): SutTypes {
+  const createAuthGroupStub = makeCreateAuthGroupRepositoryStub()
+  const sut = new DbCreateAuthGroup(createAuthGroupStub)
+
+  return {
+    sut,
+    createAuthGroupStub
+  }
+}
+
+function makeCreateAuthGroupRepositoryStub (): CreateAuthGroupRepository {
+  class CreateAuthGroupRepositoryStub implements CreateAuthGroupRepository {
+    async create (authGroup: AuthGroupProperties): Promise<AuthGroup> {
+      return mockedAuthGroup
+    }
+  }
+  return new CreateAuthGroupRepositoryStub()
+}
+
+const mockedAuthGroup = mockAuthGroup()
+function mockAuthGroup (): AuthGroup {
+  const activity = {
+    name: faker.name.title(),
+    permissions: [
+      faker.lorem.word(5),
+      faker.lorem.word(5),
+      faker.lorem.word(5)
+    ]
+  }
+  return {
+    id: <uuid>faker.datatype.uuid(),
+    title: faker.name.title(),
+    activities: [activity]
+  }
+}
+
+const mockedAuthGroupProperties = mockAuthGroupProperties()
+function mockAuthGroupProperties (): AuthGroupProperties {
+  const data = Object.assign({}, mockedAuthGroup)
+  Reflect.deleteProperty(data, 'id')
+  return data
+}
--- src/use-case/auth-group/db-create-auth-group.ts
+++ src/use-case/auth-group/db-create-auth-group.ts
@@ -1,7 +1,11 @@
 import { AuthGroup, AuthGroupProperties, CreateAuthGroup } from '@/entity/auth-group'
+import { CreateAuthGroupRepository } from './protocols/create-auth-grup-repository'
 
 export class DbCreateAuthGroup implements CreateAuthGroup {
+  constructor (private readonly createAuthGroupStub: CreateAuthGroupRepository) {}
+
   async create (authGroup: AuthGroupProperties): Promise<AuthGroup> {
+    await this.createAuthGroupStub.create(authGroup)
     return null
   }
 }
